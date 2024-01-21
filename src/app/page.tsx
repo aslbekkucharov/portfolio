585731aeaee306022ebee5c6f09b84bed72461a9
Aslbek Kucharov <aslbekkucharov@gmail.com>
2024-01-21 23:35:35 +0500

    refactor: additional info added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { User } from '@/components/User'
 import { TechStack } from '@/components/TechStack'
 import { userTechStack, workExpirience } from '@/config'
 import { WorkExperience } from '@/components/WorkExperienceItem'
+import { LogosTelegram } from '@/components/icons/LogosTelegram'
 
 export default function Home() {
 
@@ -14,14 +15,23 @@ export default function Home() {
           <User />
 
           <div className="flex flex-col items-start justify-start">
-            <h6 className="inline-block text-white text-sm mb-2">About me 🐱‍💻</h6>
-            <p className="text-slate-400 text-sm mb-3">Frontend developer with more than 3+ years experience. I enjoy to create intuitive interfaces, clean/readable code. Keeping on top of the wave with all the popular development tools. Due to the fact that I love my job, I try to do everything as perfectly as possible.</p>
-            <TechStack className='text-slate-300' technologies={userTechStack} />
+            <h6 className="inline-block text-white text-sm mb-2">About me 💻</h6>
+            <p className="text-slate-400 text-sm mb-3">
+              Frontend developer with more than 3+ years experience. I enjoy to create intuitive interfaces, clean/readable code. 
+              Keeping on top of the wave with all the popular development tools. 
+              Due to the fact that I love my job, I try to do everything as perfectly as possible.
+            </p>
+            <div className='inline-flex items-center gap-2 mb-6'>
+              <span className='text-[#28a8e9] text-sm'>For over 2.5 years now, member of Telegram Support team (TSF)</span>
+            </div>
+
+            <span className='inline-block text-white text-sm mb-2'>Professionally owned 🔥</span>
+            <TechStack className='text-slate-400' technologies={userTechStack} />
           </div>
         </section>
 
         <section className='flex flex-col gap-8'>
-          <h6 className="text-white text-sm">Work Experience 📠</h6>
+          <h6 className="text-slate-400 text-sm">Work Experience ⚒</h6>
 
           {workExpirience.map((exp, i) => (<WorkExperience experience={exp} key={i} />))}
         </section>
--- src/components/icons/IonDocumentText.tsx
+++ src/components/icons/IonDocumentText.tsx
@@ -1,4 +1,6 @@
-export default function IonDocumentText(props: any) {
+import { SVGProps } from "react"
+
+export default function IonDocumentText(props: SVGProps<SVGSVGElement>) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512" {...props}>
             <path fill="currentColor" d="M428 224H288a48 48 0 0 1-48-48V36a4 4 0 0 0-4-4h-92a64 64 0 0 0-64 64v320a64 64 0 0 0 64 64h224a64 64 0 0 0 64-64V228a4 4 0 0 0-4-4m-92 160H176a16 16 0 0 1 0-32h160a16 16 0 0 1 0 32m0-80H176a16 16 0 0 1 0-32h160a16 16 0 0 1 0 32"></path>
