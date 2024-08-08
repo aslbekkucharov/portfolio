812943d9fd943630d63d8b01513b5b138e29be2a
Kucharov Aslbek <kuas@corp.muk.ua>
2024-08-08 11:48:24 +0500

    refactor: add key prop to posts loading skeleton

--- app/posts/loading.tsx
+++ app/posts/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@nextui-org/skeleton"
 function PostsPageSkeleton() {
     return (
         <div className="grid grid-cols-2 gap-3">
-            {Array.from({ length: 6 }, (x, i) => i).map(i => <Skeleton className="h-60 w-full rounded-large" />)}
+            {Array.from({ length: 6 }, (x, i) => i).map(i => <Skeleton key={i} className="h-60 w-full rounded-large" />)}
         </div>
     )
 }
