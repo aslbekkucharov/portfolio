40f04b2010bdcc5710cc8b8fd2147a4755589106
Kucharov Aslbek <kuas@corp.muk.ua>
2024-08-13 15:49:30 +0500

    chore: experience text edits

--- config/experiences.ts
+++ config/experiences.ts
@@ -4,7 +4,7 @@ export const workExpirience: IWorkExperience[] = [
     {
         period: '04.04.2022 - 01.04.2024',
         company_link: 'https://bulavka.uz',
-        company: 'Bulavka.uz | Middle Frontend Developer',
+        company: 'Bulavka.uz | Frontend Developer',
         tech_stack: ['NuxtJS', 'VueJS', 'AngularJS', 'NodeJS', 'TailwindCSS'],
         responsibility:
             'Bulavka.uz - one of the major online marketplaces in Uzbekistan. In this company, I hold the position of a mid/senior Frontend developer. In this company I keep the website up and running, implement new features, fix bugs. I am also doing development on the admin panel and business cabinet for product suppliers. So far I have refactored a lot of code and optimized the business cabinet and administrative panel.',
@@ -12,7 +12,7 @@ export const workExpirience: IWorkExperience[] = [
     {
         period: '01.05.2021 - 04.04.2022',
         company_link: 'https://kirano.uz',
-        company: 'Kirano.uz | Junior Frontend Developer',
+        company: 'Kirano.uz | Frontend Developer',
         tech_stack: [
             'HTML',
             'SCSS',
@@ -28,7 +28,7 @@ export const workExpirience: IWorkExperience[] = [
     {
         period: '01.02.2021 - 28.04.2021',
         company_link: '',
-        company: 'Webtuning | Junior Frontend Developer',
+        company: 'Webtuning | HTML Layout Designer',
         tech_stack: ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'jQuery'],
         responsibility:
             'Webtuning is a new web studio at that time. I learned a lot here, we worked with PHP code (layout of the template by design in Figma). I got acquainted with such concepts as FTP, Git, etc.',
