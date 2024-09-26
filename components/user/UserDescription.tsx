ab99ae782ce70f9e810eca698aaa102e854b3bbf
Kucharov Aslbek <kuas@corp.muk.ua>
2024-09-26 14:13:55 +0500

    chore: user description changed

--- components/user/UserDescription.tsx
+++ components/user/UserDescription.tsx
@@ -2,7 +2,7 @@ function UserDescription() {
     return (
         <div className="flex flex-col gap-y-2">
             <p className="text-slate-700 dark:text-slate-300">
-                Experienced Frontend developer with a strong interest in backend development. I specialize in creating high-quality user interfaces using modern technologies and frameworks (React/Vue). I am actively learning Node.js (Nest.js) to expand my skills and create more complex applications. In my free time, I am passionate about mountain hiking and extreme sports, which helps me stay focused and pay attention to details in my work.
+                I'm a dedicated frontend developer with a growing passion for backend technologies, currently diving deep into Node.js (Nest.js) to broaden my skillset. With hands-on experience in modern frameworks like React and Vue, I craft intuitive, high-quality user interfaces that prioritize both performance and design. Outside of coding, I'm an avid mountain hiker and extreme sports enthusiast, which fuels my attention to detail and problem-solving mindset.
             </p>
             <p className="text-[#28a8e9] font-medium">
                 I have been a member of the Telegram support team for more than 3 years.
