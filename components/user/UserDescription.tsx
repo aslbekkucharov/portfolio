80388dd4609f0436d7b8c11fa26bbe1c487e2f45
Kucharov Aslbek <kuas@corp.muk.ua>
2024-09-26 15:38:53 +0500

    fix: symbolic error during build step

--- components/user/UserDescription.tsx
+++ components/user/UserDescription.tsx
@@ -2,7 +2,7 @@ function UserDescription() {
     return (
         <div className="flex flex-col gap-y-2">
             <p className="text-slate-700 dark:text-slate-300">
-                I'm a dedicated frontend developer with a growing passion for backend technologies, currently diving deep into Node.js (Nest.js) to broaden my skillset. With hands-on experience in modern frameworks like React and Vue, I craft intuitive, high-quality user interfaces that prioritize both performance and design. Outside of coding, I'm an avid mountain hiker and extreme sports enthusiast, which fuels my attention to detail and problem-solving mindset.
+            I&apos;m a dedicated frontend developer with a growing passion for backend technologies, currently diving deep into Node.js (Nest.js) to broaden my skillset. With hands-on experience in modern frameworks like React and Vue, I craft intuitive, high-quality user interfaces that prioritize both performance and design. Outside of coding, I&apos;m an avid mountain hiker and extreme sports enthusiast, which fuels my attention to detail and problem-solving mindset.
             </p>
             <p className="text-[#28a8e9] font-medium">
                 I have been a member of the Telegram support team for more than 3 years.
