99db8689f442f360b023043742d4d7256f77ba09
Kucharov Aslbek <kuas@corp.muk.ua>
2024-10-04 14:37:04 +0500

    refactor: add horizontal margins for experience block

--- components/ExperienceBlock.tsx
+++ components/ExperienceBlock.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 function ExperienceBlock({ experience }: Props) {
     return (
-        <div className="grid sm:grid-cols-[200px_1fr] gap-2 sm:gap-0">
+        <div className="grid sm:grid-cols-[200px_1fr] gap-2 sm:gap-0 first:mt-0 last:mb-0 my-5">
             <span className="text-slate-700 dark:text-slate-300">{experience.period}</span>
             <div className="flex flex-col gap-3">
                 <Link isExternal showAnchorIcon anchorIcon={<LinkOutlineIcon className="text-white" size={24} viewBox="0 0 22 22" />} className="inline-flex items-center gap-2 group" href={experience.company_link}>
